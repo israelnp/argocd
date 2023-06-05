1e31aff8ea83213add4bedd2431585b6718f8bfd
israelnp <israelpereira1984@gmail.com>
2023-06-05 17:06:56 -0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route("/")
+def hello_world():
+
+    return "gg du bom!"
+
+@app.route("/cadastro")
+def cadastro():
+
+    return "cadastro"
+
+@app.route("/produto")
+def produto():
+
+    return "produto"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug = False)
